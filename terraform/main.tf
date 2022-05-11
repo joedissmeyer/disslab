90e56b8c93807db24fd0e9f5cde3979f2d97e547
jdi6755 <jdissmeyer@carnival.com>
2022-05-10 21:12:17 -0400

    Adding terraform for testing

---
+++ terraform/main.tf
@@ -0,0 +1,231 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "2.9.10"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://192.168.0.200:8006/api2/json"
+  pm_api_token_id = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure = true
+}
+
+# Provision the FREEIPA VM
+resource "proxmox_vm_qemu" "ipaserver" {
+  count = 1
+  name = "freeipa"
+  target_node = var.proxmox_host
+  clone = var.template_name
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot = 0
+    size = "50G"
+    type = "scsi"
+    storage = "Data"
+    iothread = 1
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  ipconfig0 = "ip=192.168.0.201/24,gw=192.168.0.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
+
+# Provision the Prometheus & Grafana VM
+resource "proxmox_vm_qemu" "prometheus" {
+  count = 1
+  name = "prometheus"
+  target_node = var.proxmox_host
+  clone = var.template_name
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot = 0
+    size = "50G"
+    type = "scsi"
+    storage = "Data"
+    iothread = 1
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  ipconfig0 = "ip=192.168.0.202/24,gw=192.168.0.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
+
+# Provision kibana VM
+resource "proxmox_vm_qemu" "elastic-kibana" {
+  count = 1
+  name = "kibana"
+  target_node = var.proxmox_host
+  clone = var.template_name
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot = 0
+    size = "32G"
+    type = "scsi"
+    storage = "Data"
+    iothread = 1
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  ipconfig0 = "ip=192.168.0.211/24,gw=192.168.0.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
+
+# Provision logstash VMs
+resource "proxmox_vm_qemu" "elastic-logstash" {
+  count = 1
+  name = "logstash"
+  target_node = var.proxmox_host
+  clone = var.template_name
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot = 0
+    size = "32G"
+    type = "scsi"
+    storage = "Data"
+    iothread = 1
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  ipconfig0 = "ip=192.168.0.212/24,gw=192.168.0.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
+
+# Provision elastic-master VMs
+resource "proxmox_vm_qemu" "elastic-master" {
+  count = 3
+  name = "elastic-master-${count.index + 1}"
+  target_node = var.proxmox_host
+  clone = var.template_name
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot = 0
+    size = "32G"
+    type = "scsi"
+    storage = "Data"
+    iothread = 1
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  ipconfig0 = "ip=192.168.0.22${count.index + 1}/24,gw=192.168.0.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
+
+# Provision the elastic-data VMs
+resource "proxmox_vm_qemu" "elastic-data" {
+  count = 3
+  name = "elastic-data-${count.index + 1}"
+  target_node = var.proxmox_host
+  clone = var.template_name
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 4096
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot = 0
+    size = "100G"
+    type = "scsi"
+    storage = "Data"
+    iothread = 1
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  ipconfig0 = "ip=192.168.0.23${count.index + 1}/24,gw=192.168.0.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,17 @@
+variable "ssh_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkMVn4NB1991Kv34Q1tJCdR2K/stQdWD4bi0ebIbdPF0TofpSOACs6yMTr6Dl5/40hUA2r7S/ae12PdxFixjNYenEnM/ZzLTSf0t/ZEQfq/+H+mMUPNj7X9rzkKmJGXh6RcxcM0p+yyCZY3GDwyaY1hRFCEQxrfZXKeij6jiVzDW2i7qLz16ilNjyQyX2vIi3d+Z5mqKgdoR2YVTlJ4f5nx1u++b3cCq8M8syn9Ss+XGDoj5tSt0oDtzo2YFaCyNXy6TLWEpDUHx1mJSwoUn3N8ZSJTO8+6gy6foNmnIWEsPH6gysAHMljV+VMg1pdm0/QEytRlkexz4G/ANtZqPJkklY9A5TGznZD2qslHKsgGatD67BRoaO86M1ziKFq/KCMH1fXO3pOy4IEAX4wuKLin7WQW2K5MHP4y3Vv6keBtafvCRCBKEAYTlAtTZ3WKqe0K3KRIOET9wrkVYmkZKVWpGgevUl5tIwlZZzUx7e6Fa+7BS76eB9FjCcpwzIC7Vc= josephdissmeyer@Josephs-MacBook-Pro.local"
+}
+variable "proxmox_host" {
+  default = "proxmox"
+}
+variable "template_name" {
+  default = "fedora-36-template"
+}
+variable "pm_api_token_id" {
+  default = "terraform-user@pam!terraform_token"
+  sensitive = true
+}
+variable "pm_api_token_secret" {
+  default = "enter-key-here"
+  sensitive = true
+}
